0b773af0b38b1a0af0e83a62287a96c20df0f604
Dev-will409 <rlaslstjr@gmail.com>
2022-05-13 16:33:33 +0900

    [Silver IV] Title: 문서 검색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.cc
+++ 백준/Silver/1543. 문서 검색/문서 검색.cc
@@ -9,29 +9,20 @@ int main() {
 
     getline(cin,doc);
     getline(cin,word);
-
-    if(word.size() > doc.size()) cout << 0;
-    else{
-        for (int j = 0; j <= doc.size() - word.size(); ++j) {
-            int i = j;
-            count = 0;
-            while (i <= doc.size() - word.size())
-            {
-                if(doc.substr(i, word.size()) == word){
-                    i += word.size();
-                    count ++;
-                }
-                else{
-                    i++;
-                }
+    int i = 0;
+        while (i < doc.size())
+        {
+            if(doc.substr(i, word.size()) == word){
+                i += word.size();
+                count ++;
             }
-            if(max < count) max = count;
+            else{
+                i++;
+            }
         }
+        if(max < count) max = count;
+
         cout << max;
-    }
-
-
-
 
 
     return 0;
