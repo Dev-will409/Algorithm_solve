3b0a5a9ed4ed3ad70f9a14afa1cd42ba2cffa4c4
김민석 <devwill409@gmail.com>
2023-01-26 15:57:48 +0900

    [Gold III] Title: 줄 세우기, Time: 40 ms, Memory: 3824 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,40 +1,42 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int inDegree[32001] = {0,};
+
 vector<int> arr[32001];
 queue<int> q;
 
 int main() {
     int n, m;
 
-    cin >> n >> m;
+    scanf("%d %d", &n, &m);
 
     for (int i = 0; i < m; ++i) {
-        int a, b;
+        int a,b;
         scanf("%d %d", &a, &b);
-        arr[a].push_back(b);
         inDegree[b] += 1;
+        arr[a].push_back(b);
     }
+
     for (int i = 1; i <= n; ++i) {
-        if(inDegree[i] == 0){
+        if(inDegree[i] == 0) {
             q.push(i);
         }
     }
-    for (int i = 0; i < n; ++i) {
-        cout << q.front() << " ";
-        int s = int(arr[q.front()].size());
-        for (int j = 0; j < s; ++j) {
-            int t = arr[q.front()][j];
-            inDegree[t] -= 1;
-            if(inDegree[t] == 0) q.push(t);
+
+    while(!q.empty()){
+        printf("%d ", q.front());
+        for (int i = 0; i < arr[q.front()].size(); ++i) {
+            inDegree[arr[q.front()][i]] -= 1;
+            if (inDegree[arr[q.front()][i]] == 0){
+                q.push(arr[q.front()][i]);
+            }
         }
         q.pop();
     }
-
 
 
     return 0;
